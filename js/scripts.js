7d5368a34b539594d45812b24b6b00c2bb16e3f0
lrutkin <lrutkin@gmail.com>
2022-04-23 22:04:20 -0700

    recognize 1 as input and display alert

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+const beep = "Beep"
+const input =  1
+const inputString = input.toString()
+
+if (inputString.includes("1")) {
+    alert(beep);
+  } else {
+    alert("Nothing happened.");
+  }
