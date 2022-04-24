c9a897e1a1bc8d0e9b338424a3a4791e90bb9fa5
lrutkin <lrutkin@gmail.com>
2022-04-23 22:12:40 -0700

    recognize 3 as input

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-
-const beep = "Beep"
-const boop = "Boop"
-const input =  2
+const beep = "Beep!"
+const boop = "Boop!"
+const neighbor = "Won't you be my neighbor?"
+const input =  3
 const inputString = input.toString()
 
 if (inputString.includes("1")) {
     alert(beep);
   } else if (inputString.includes("2")) {
     alert(boop);
+  } else if (inputString.includes("3")) {
+    alert(neighbor);
   } else {
     alert("Nothing happened.");
   }
