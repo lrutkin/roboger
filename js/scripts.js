938126fd0e1a6b06c04f2da1c8be941e6b233686
lrutkin <lrutkin@gmail.com>
2022-04-23 22:08:09 -0700

    recognize 2 as input

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 
 const beep = "Beep"
-const input =  1
+const boop = "Boop"
+const input =  2
 const inputString = input.toString()
 
 if (inputString.includes("1")) {
     alert(beep);
+  } else if (inputString.includes("2")) {
+    alert(boop);
   } else {
     alert("Nothing happened.");
   }
